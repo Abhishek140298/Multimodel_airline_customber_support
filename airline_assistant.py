a47cb6cf55bb52fe76c396adfce5edad75ada1bc
Abhishek140298 <abhishekyadavfeb1498@gmail.com>
2026-08-19 21:10:11 +0530

    Added the function call

--- airline_assistant.py
+++ airline_assistant.py
@@ -40,10 +40,18 @@ tools=[{"type":"function","function":price_function}]
 def chat(message,history):
     # 'history' is a list of past {"role": ..., "content": ...} messages Gradio manages for us
     messages=[{"role":"system","content":system_prompt}]+history+[{{"role": "user", "content": message}}]
-    response=cleint.chat.completion.create(model="gpt-4o-mini",message=messages,tools=tools)
+    response=cleint.chat.completion.create(model="gpt-4o-mini",messages=messages,tools=tools)
     if response.choices[0].finish_reason=="tool_calls":
         tool_call_message=response.choices[0].message
         messages.append(tool_call_message)  # record that the model asked for a tool call
+        for tool_call in tool_call_message.tool_calls:
+            if tool_call.function.name=="get_ticket_price":
+               args=json.loads( tool_call.function.arguments)
+               city=args.get("destination_city")
+               price=get_ticket_price(city)
+               messages.append({"role":"tool", "content": json.dumps({"destination_city": city, "price": price}),
+                    "tool_call_id": tool_call.id})
+        response=cliet.chat.completion.create(model="gpt-4o-mini",messages=messages)       
 
     
     return response.choices[0].message.content
